6bb877fe45735a3c4cb0cf2f856c33ddb7d58a8a
unknown <pablo.rotela@gmail.com>
2020-02-27 01:16:39 -0300

    rehice parcial punto 7

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,82 @@
 function mostrar()
 {
+    var CantidadAlumnos = 1;
+    var nota = 0;
+    var sexo;
+    var valido;
+    var sumaNotas = 0;
+    var notaMinima = 0;
+    var sexoNotaMinima;
+    var promedioNotas = 0;
+    var varonesNotaMayor6 = 0;
+    do
+    {
+        do
+        {
+            valido = true;
+            nota = prompt("Ingrese la nota del alumno " + CantidadAlumnos.toString());
+            if(isNaN(nota))
+            {
+                alert("Favor de ingresar numeros");
+                valido = false;
+            }
+            nota = parseInt(nota);
+            if(nota <0 || nota > 10)
+            {
+                alert("Entre 0 y 10 por favor");
+                valido = false;
+            }
+        }
+        while(!valido);
+
+
+        do
+        {
+            valido = true;
+            sexo = prompt("Ingrese el sexo ( F o M ) del alumno" + CantidadAlumnos.toString());
+            sexo = sexo.toUpperCase();
+            if(sexo != "F" && sexo != "M")
+            {
+                alert("Favor ingrese F o M ");
+                valido = false;
+            }
+           
+           
+        }
+        while(!valido);
+
+        sumaNotas += nota;
+
+        if(CantidadAlumnos == 1)
+        {
+            notaMinima = nota;
+            sexoNotaMinima = sexo;
+        }
+        else
+        {
+            if( notaMinima > nota)
+            {
+                notaMinima = nota;
+                sexoNotaMinima = sexo;
+            }
+        }
+
+        if(sexo == "M")
+        {
+            if(nota >= 6)
+            {
+                varonesNotaMayor6++;
+            }
+        }
+
+        CantidadAlumnos ++;
+    }
+    while(CantidadAlumnos <= 5);
+    
+    promedioNotas = sumaNotas / 5;
+    alert(" Promedio de notas totales " + promedioNotas);
+    alert(" Nota mas baja " + notaMinima.toString() + " y el sexo fue " + sexoNotaMinima.toString());
+    alert(" Varones cuya nota es mayor o igual a 6 " + varonesNotaMayor6);
+
 
 }
